53c240476c47323b1d0114d84dfb8faf7e1eb67e
Dennis D. Silva <dsilva609@gmail.com>
2020-07-22 23:30:39 -0400

    feat(nav): add max width for nav bar

--- src/app/shared/nav/nav.component.ts
+++ src/app/shared/nav/nav.component.ts
@@ -11,6 +11,7 @@ import { UserService } from 'src/app/user/state/user.service';
 })
 export class NavComponent implements OnInit {
   isCollapsed: boolean;
+
   constructor(
     private authService: AuthService,
     private router: Router,
