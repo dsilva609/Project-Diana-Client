3b9e9e655c67ec676a0a96e1ccfe839dcf66e39c
Dennis D. Silva <dsilva609@gmail.com>
2020-07-23 22:34:12 -0400

    build: enable akita for prod

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
-import { akitaConfig } from '@datorama/akita';
 import { enableProdMode } from '@angular/core';
-import { persistState } from '@datorama/akita';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {
+  akitaConfig,
+  enableAkitaProdMode,
+  persistState,
+} from '@datorama/akita';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+  enableAkitaProdMode();
 }
 
 akitaConfig({
