7b290006ae7284c7f54d09b5b08a71cdf073f507
Dennis D. Silva <dsilva609@gmail.com>
2020-07-28 20:16:04 -0400

    build(app): use hash location

--- Project-Diana-Client/src/app/app.module.ts
+++ Project-Diana-Client/src/app/app.module.ts
@@ -1,22 +1,23 @@
+import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
+import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
+import { AppComponent } from 'src/app/app.component';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HttpRequestInterceptor } from 'src/shared/http/http-request.interceptor';
+import { JwtModule } from '@auth0/angular-jwt';
+import { LoginComponent } from 'src/app/user/login/login.component';
+import { NavComponent } from 'src/app/shared/nav/nav.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { JwtModule } from '@auth0/angular-jwt';
-import { AkitaNgRouterStoreModule } from '@datorama/akita-ng-router-store';
-import { AkitaNgDevtools } from '@datorama/akita-ngdevtools';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { AppComponent } from 'src/app/app.component';
-import { NavComponent } from 'src/app/shared/nav/nav.component';
-import { LoginComponent } from 'src/app/user/login/login.component';
 import { UserComponent } from 'src/app/user/user.component';
 import { WishListComponent } from 'src/app/wish/wish-list/wish-List.component';
 import { WishOwnedComponent } from 'src/app/wish/wish-list/wish-owned/wish-owned.component';
-import { environment } from 'src/environments/environment';
-import { HttpRequestInterceptor } from 'src/shared/http/http-request.interceptor';
 
 @NgModule({
   declarations: [
@@ -45,6 +46,7 @@ import { HttpRequestInterceptor } from 'src/shared/http/http-request.interceptor
       useClass: HttpRequestInterceptor,
       multi: true,
     },
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
   ],
   bootstrap: [AppComponent],
 })
