21a750fff6e966daf643b5e99a0cc698fd7599e1
Dennis D. Silva <dsilva609@gmail.com>
2020-07-29 21:23:02 -0400

    project: update environment to use variables

--- Project-Diana-Client/src/environments/environment.ts
+++ Project-Diana-Client/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiBaseUrl: 'https://localhost:44334',
-  production: false,
+  apiBaseUrl: '#{apiBaseUrl}#',
+  production: '#{production}#',
 };
 
 /*
