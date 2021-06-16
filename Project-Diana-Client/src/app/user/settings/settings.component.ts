01dbd711d4e0610fbf6e09a93eaff74d5add16e6
Dennis D. Silva <dsilva609@gmail.com>
2021-06-16 00:29:58 -0400

    feat(user): add display name edit field

--- Project-Diana-Client/src/app/user/settings/settings.component.ts
+++ Project-Diana-Client/src/app/user/settings/settings.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  userDisplayName = 'name';
+  isDisplayNameEditing = false;
+  @ViewChild('displayName', { static: false })
+  displayNameTxt: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onDisplayNameCancelEdit(): void {
+    this.isDisplayNameEditing = false;
   }
 
+  onDisplayNameEdit(): void {
+    this.isDisplayNameEditing = true;
+    this.displayNameTxt.nativeElement.value = this.userDisplayName;
+  }
+
+  onDisplayNameSave(): void {
+    this.userDisplayName = this.displayNameTxt.nativeElement.value;
+    this.isDisplayNameEditing = false;
+  }
 }
