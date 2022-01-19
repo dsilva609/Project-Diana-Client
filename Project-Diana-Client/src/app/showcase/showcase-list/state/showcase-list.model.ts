994f68de4e00cdf31ceb2a0f1413ab70d995c8ba
Dennis D Silva <dsilva609@gmail.com>
2022-01-18 22:46:32 -0500

    feat(showcase): add games and movies to showcase

--- Project-Diana-Client/src/app/showcase/showcase-list/state/showcase-list.model.ts
+++ Project-Diana-Client/src/app/showcase/showcase-list/state/showcase-list.model.ts
@@ -1,14 +1,20 @@
 import { Album } from 'src/app/album/album.model';
 import { Book } from 'src/app/book/book.model';
+import { Game } from 'src/app/game/game.model';
+import { Movie } from 'src/app/movie/movie.model';
 
 export interface ShowcaseListResponse {
   showcasedAlbums: Album[];
   showcasedBooks: Book[];
+  showcasedGames: Game[];
+  showcasedMovies: Movie[];
 }
 
 export function createShowcaseList(): ShowcaseListResponse {
   return {
     showcasedAlbums: [],
     showcasedBooks: [],
+    showcasedGames: [],
+    showcasedMovies: [],
   } as ShowcaseListResponse;
 }
--- Project-Diana-Client/src/app/showcase/showcase-list/state/showcase-list.store.ts
+++ Project-Diana-Client/src/app/showcase/showcase-list/state/showcase-list.store.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@angular/core';
 import { EntityState, EntityStore, StoreConfig } from '@datorama/akita';
 import { Album } from 'src/app/album/album.model';
 import { Book } from 'src/app/book/book.model';
+import { Game } from 'src/app/game/game.model';
+import { Movie } from 'src/app/movie/movie.model';
 import { createShowcaseList, ShowcaseListResponse } from 'src/app/showcase/showcase-list/state/showcase-list.model';
 
 export interface ShowcaseListState extends EntityState<ShowcaseListResponse> {
   showcasedAlbums: Album[];
   showcasedBooks: Book[];
+  showcasedGames: Game[];
+  showcasedMovies: Movie[];
 }
 
 @Injectable({ providedIn: 'root' })
