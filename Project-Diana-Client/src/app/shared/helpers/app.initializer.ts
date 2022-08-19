42f342bf05bc8c0f2e98bb29f1c8cf453d8eb159
Dennis D. Silva <dsilva609@gmail.com>
2022-08-18 22:48:36 -0400

    build(deps): update packages

--- Project-Diana-Client/src/app/shared/helpers/app.initializer.ts
+++ Project-Diana-Client/src/app/shared/helpers/app.initializer.ts
@@ -2,19 +2,16 @@ import { switchMap, take } from 'rxjs/operators';
 import { UserQuery } from 'src/app/user/state/user.query';
 import { UserService } from 'src/app/user/state/user.service';
 
-// tslint:disable-next-line: typedef
 export function appInitializer(userQuery: UserQuery, userService: UserService) {
-  return () =>
-    new Promise((resolve) => {
-      userQuery
-        .select()
-        .pipe(
-          take(1),
-          switchMap((user) => {
-            return userService.refreshToken(user.refreshToken);
-          })
-        )
-        .subscribe()
-        .add(resolve);
-    });
+  return () => {
+    userQuery
+      .select()
+      .pipe(
+        take(1),
+        switchMap((user: { refreshToken: string }) => {
+          return userService.refreshToken(user.refreshToken);
+        })
+      )
+      .subscribe();
+  };
 }
